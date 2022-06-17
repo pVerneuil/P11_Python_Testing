82b65600e09bea0cd116f8eaad9d91790b10a66e
Pverneuil <verneuil.paul@gmail.com>
2022-06-17 13:17:45 +0200

    bugfix: issu#4 Clubs shouldn't be able to book more than 12 places per competition #4

--- server.py
+++ server.py
@@ -6,16 +6,17 @@ from flask import Flask,render_template,request,redirect,flash,url_for
 current_path = Path(__file__).absolute().parent
 PATH_CLUBS = current_path / "data/clubs.json"
 PATH_COMPETITIONS = current_path / "data/competitions.json"
+
 def loadClubs():
     with open(PATH_CLUBS) as c:
-         listOfClubs = json.load(c)['clubs']
-         return listOfClubs
+        listOfClubs = json.load(c)['clubs']
+        return listOfClubs
 
 
 def loadCompetitions():
     with open(PATH_COMPETITIONS) as comps:
-         listOfCompetitions = json.load(comps)['competitions']
-         return listOfCompetitions
+        listOfCompetitions = json.load(comps)['competitions']
+        return listOfCompetitions
 
 
 app = Flask(__name__)
@@ -23,6 +24,20 @@ app.secret_key = 'something_special'
 
 competitions = loadCompetitions()
 clubs = loadClubs()
+"""
+track how many places are already reserved by clubs in each competition.
+reserved_place ={'competition1_name': {
+                        'club1_name': place booked,
+                        'club2_name': place booked, ...
+                                    },
+                'competition2_name': {
+                        'club1_name': place booked,
+                        'club2_name': place booked, ...
+                                    }, ...
+                }
+"""
+reserved_places = {competition['name']:{club['name']:0 for club in clubs} for competition in competitions }
+
 
 @app.route('/')
 def index():
@@ -55,15 +70,23 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
-    
-    if int(club['points'])<placesRequired:
-        message = f"You do not have enough points to perfom this action ( you have {club['points']} point(s))"
+    print(reserved_places[competition['name']][club['name']])
+
+    if placesRequired<=12 and placesRequired+reserved_places[competition['name']][club['name']]<= 12 :
+        
+        if int(club['points'])<placesRequired:
+            message = f"You do not have enough points to perfom this action ( you have {club['points']} point(s))"
+            return render_template('booking.html',club=club,competition=competition, message=message), 400
+        else :
+            competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+            club['points'] = int(club['points'])-placesRequired
+            reserved_places[competition['name']][club['name']] += placesRequired
+            flash('Great-booking complete!')
+            return render_template('welcome.html', club=club, competitions=competitions)
+        
+    else : 
+        message ='You can not book more than 12 places per competition' 
         return render_template('booking.html',club=club,competition=competition, message=message), 400
-    else :
-        competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-        club['points'] = int(club['points'])-placesRequired
-        flash('Great-booking complete!')
-        return render_template('welcome.html', club=club, competitions=competitions)
 
 
 # TODO: Add route for points display
--- tests/unit_tests/test_purchasePlaces.py
+++ tests/unit_tests/test_purchasePlaces.py
@@ -18,7 +18,7 @@ class TestPurshase:
         
         assert response.status_code == 400
         assert message in response.data.decode('UTF-8')
-    
+
 
     def test_club_points_should_be_deducted_after_redeemed(self, client,testing_data):
         """
@@ -40,3 +40,91 @@ class TestPurshase:
 
         assert response.status_code == 200
         assert int(club['points']) == expected_values['number_of_club_points_updated']
+    
+    def test_places_should_be_deducted_from_the_competition_when_redeemed(self, client, testing_data):
+        """
+        test issue#4 : club's should not be able to book more than 12 places per competitons
+        """
+        club = testing_data['clubs'][2] 
+        competition = testing_data['competitions'][2]
+        number_of_place_club_try_to_redeem = 12
+        expected_values ={
+            'number_of_place_in_competitions_updated' : int(competition['numberOfPlaces'])- number_of_place_club_try_to_redeem
+        }
+        response1 = client.post(
+            '/purchasePlaces', data={
+            'club' : club['name'] , 
+            'competition': competition['name'],
+            'places':number_of_place_club_try_to_redeem
+            })
+        
+        assert response1.status_code == 200
+        assert int(competition['numberOfPlaces']) == expected_values['number_of_place_in_competitions_updated']
+        
+    def test_club_should_be_able_to_book_12_or_less_place_per_competition(self,client,testing_data):
+        """
+        test issue#4 : club's should not be able to book more than 12 places per competitons
+        """
+        club = testing_data['clubs'][2] 
+        competition = testing_data['competitions'][2]
+        number_of_place_club_try_to_redeem = 12
+        
+        response1 = client.post(
+            '/purchasePlaces', data={
+            'club' : club['name'] , 
+            'competition': competition['name'],
+            'places':number_of_place_club_try_to_redeem
+            })
+        
+        assert response1.status_code == 200
+
+    def test_club_should_not_be_able_to_book_more_than_12_place_per_competition_at_once(self,client,testing_data):
+        """
+        test issue#4 : club's should not be able to book more than 12 places per competitons
+        """
+        club = testing_data['clubs'][4] 
+        competition = testing_data['competitions'][0]
+        number_of_place_club_try_to_redeem = 13
+        
+        response = client.post(
+            '/purchasePlaces', data={
+            'club' : club['name'] , 
+            'competition': competition['name'],
+            'places':number_of_place_club_try_to_redeem
+            })
+        message ='You can not book more than 12 places per competition' 
+        
+        assert response.status_code == 400
+        assert message in response.data.decode('UTF-8')
+
+    def test_club_should_not_be_able_to_book_more_than_12_place_per_competition_in_multiple_time(self,client,testing_data):
+        """
+        test issue#4 : club's should not be able to book more than 12 places per competitons
+        """
+        club = testing_data['clubs'][0] 
+        competition = testing_data['competitions'][1]
+        number_of_place_club_try_to_redeem = 6
+        
+        response1 = client.post(
+            '/purchasePlaces', data={
+            'club' : club['name'] , 
+            'competition': competition['name'],
+            'places':number_of_place_club_try_to_redeem
+            })
+        
+        assert response1.status_code == 200
+        
+        club = testing_data['clubs'][0] 
+        competition = testing_data['competitions'][1]
+        number_of_place_club_try_to_redeem = 7
+        
+        response2 = client.post(
+            '/purchasePlaces', data={
+            'club' : club['name'] , 
+            'competition': competition['name'],
+            'places':number_of_place_club_try_to_redeem
+            })
+        message ='You can not book more than 12 places per competition' 
+        
+        assert response2.status_code == 400
+        assert message in response2.data.decode('UTF-8')
